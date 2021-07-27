a20a294a583102d65a2919650964ced9c682fb76
Manu NALEPA <nalepae@gmail.com>
2021-07-27 10:55:46 +0200

    Implement `pad_and_sample`

--- huge_csv_reader/pad_and_sample.py
+++ huge_csv_reader/pad_and_sample.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import os
 import hashlib
+from typing import List, Tuple
+from huge_csv_reader.pad import pad
+from huge_csv_reader.padded_text_file import padded_text_file
+from huge_csv_reader.sample import sample, sample_sampled
 
 
 def pseudo_hash(path: Path, string: str = "") -> str:
@@ -13,3 +17,51 @@ def pseudo_hash(path: Path, string: str = "") -> str:
     """
     string = "-".join([str(os.path.getsize(path)), str(os.path.getmtime(path)), string])
     return str(hashlib.md5(bytes(string, "utf-8")).hexdigest())
+
+
+def pad_and_sample(
+    source_csv_file_path: Path,
+    dest_dir_path: Path,
+    x: str,
+    ys_and_types: List[Tuple[str, type]],
+) -> None:
+    """Pad and sample `source_csv_file_path` into `dest_dir_path` with `x`.
+    
+    If the file is already sampled, this function does not resample it but exits
+    immediately without error.
+    """
+    dir_path = dest_dir_path / pseudo_hash(source_csv_file_path, x)
+
+    try:
+        dir_path.mkdir(parents=True)
+    except FileExistsError:
+        return
+
+    padded_path = dir_path / "0.csv"
+    sampled_path = dir_path / "1.csv"
+
+    pad(source_csv_file_path, padded_path)
+
+    with padded_text_file(padded_path, offset=1) as ptf:
+        nb_lines = len(ptf)
+
+    if nb_lines > 1:
+        sample(source_csv_file_path, sampled_path, x, ys_and_types, period=2)
+        pad(sampled_path)
+
+    with padded_text_file(sampled_path, offset=1) as ptf:
+        nb_lines = len(ptf)
+
+    epoch = 2
+
+    while nb_lines > 1:
+        new_sampled_path = dir_path / f"{epoch}.csv"
+        sample_sampled(sampled_path, new_sampled_path, period=2)
+
+        sampled_path = new_sampled_path
+        pad(sampled_path)
+
+        with padded_text_file(sampled_path, offset=1) as ptf:
+            nb_lines = len(ptf)
+
+        epoch += 1
--- tests/test_pad_and_sample.py
+++ tests/test_pad_and_sample.py
@@ -1,7 +1,11 @@
+import filecmp
 import os
 from pathlib import Path
 
-from huge_csv_reader.pad_and_sample import pseudo_hash
+from huge_csv_reader.pad_and_sample import pad_and_sample, pseudo_hash
+from pytest import fixture
+
+from tests import assets
 
 
 def test_pseudo_hash(tmp_path: Path):
@@ -13,3 +17,46 @@ def test_pseudo_hash(tmp_path: Path):
 
     assert pseudo_hash(path) == "46141dc64506ddc74fcf3cae348e0d77"
     assert pseudo_hash(path, "12345") == "ece35c8647c0fe39e450443c9514b412"
+
+
+@fixture
+def not_padded_file_path():
+    path = Path(assets.__file__).parent / "not_padded.csv"
+    os.utime(path, (42, 42))
+    return path
+
+
+@fixture
+def hashed():
+    return Path(assets.__file__).parent / "8bed00c4529bfd12bd70678a71eaf5af"
+
+
+@fixture
+def _0(hashed: Path):
+    return hashed / "0.csv"
+
+
+@fixture
+def _1(hashed: Path):
+    return hashed / "1.csv"
+
+
+@fixture
+def _2(hashed: Path):
+    return hashed / "2.csv"
+
+
+@fixture
+def _3(hashed: Path):
+    return hashed / "3.csv"
+
+
+def test_pad_and_sample(
+    tmp_path: Path, not_padded_file_path: Path, _0: Path, _1: Path, _2: Path, _3: Path
+):
+    pad_and_sample(not_padded_file_path, tmp_path, "a", [("b", float), ("d", float)])
+    assert filecmp.cmp(tmp_path / "8bed00c4529bfd12bd70678a71eaf5af" / "0.csv", _0)
+    assert filecmp.cmp(tmp_path / "8bed00c4529bfd12bd70678a71eaf5af" / "1.csv", _1)
+    assert filecmp.cmp(tmp_path / "8bed00c4529bfd12bd70678a71eaf5af" / "2.csv", _2)
+    assert filecmp.cmp(tmp_path / "8bed00c4529bfd12bd70678a71eaf5af" / "3.csv", _3)
+    pad_and_sample(not_padded_file_path, tmp_path, "a", [("b", float), ("d", float)])
