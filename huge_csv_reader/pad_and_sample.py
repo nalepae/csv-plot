98e89fc1ce699708a1d984c81ea817b536d76b36
Manu NALEPA <nalepae@gmail.com>
2021-08-22 17:18:46 +0200

    Refactor

--- huge_csv_reader/pad_and_sample.py
+++ huge_csv_reader/pad_and_sample.py
@@ -1,10 +1,213 @@
+import csv
+import hashlib
+import os
+from collections import defaultdict
 from pathlib import Path
-from typing import List, Set, Tuple
+from shutil import copy2
+from tempfile import NamedTemporaryFile
+from typing import Dict, List, Optional, Set, Tuple
 
-from huge_csv_reader.pad import pad
 from huge_csv_reader.padded_text_file import padded_text_file
-from huge_csv_reader.pseudo_hash import pseudo_hash
-from huge_csv_reader.sample import sample, sample_sampled
+
+
+def pseudo_hash(path: Path, string: str = "") -> str:
+    """Compute a pseudo hash based on :
+    - The file size
+    - The file last modificationdate
+    - A given string
+
+    (Real hash is too long to compute for big file.)
+    """
+    string = "-".join([str(os.path.getsize(path)), str(os.path.getmtime(path)), string])
+    return str(hashlib.md5(bytes(string, "utf-8")).hexdigest())
+
+
+def sample(
+    source_path: Path,
+    dest_path: Path,
+    x: str,
+    ys_and_types: List[Tuple[str, type]],
+    period: int,
+) -> None:
+    """Sample a CSV file every `period` line.
+
+    Example:
+    With the following CSV corresponding to `source_path`:
+    x,a,b,c,d
+    1,2,3,4,5
+    2,3,0,7,6
+    3,4,7,4,8
+    4,8,6,4,3
+    5,9,8,7,6
+    6,5,7,8,2
+
+    With x == "x", ys_and_types == [("b", float), ("d", float)] and period == 3,
+
+    The CSV file written in `dest_path` will be:
+    x,d_min,d_max,b_min,b_max
+    1,5.0,8.0,0.0,7.0
+    4,2.0,6.0,6.0,8.0
+    """
+    ys = {y for y, _ in ys_and_types}
+
+    with source_path.open() as source_file, dest_path.open("w") as dest_file:
+        if {type for _, type in ys_and_types} != {float}:
+            raise ValueError(
+                "Only `float` is allowed as a type type for `ys_and_types`"
+            )
+
+        reader = csv.DictReader(source_file)
+
+        if reader.fieldnames is None or x not in reader.fieldnames:
+            raise IndexError(f"Variable x: {x} not found in CSV file")
+
+        if not ys < set(reader.fieldnames):
+            raise IndexError(
+                f"At least on y variable is not available in the input file"
+            )
+
+        writer = csv.DictWriter(
+            dest_file,
+            lineterminator="\n",
+            fieldnames=[x]
+            + [
+                item
+                for sublist in [
+                    [f"{y}_min", f"{y}_max"] for y in (y for y, _ in ys_and_types)
+                ]
+                for item in sublist
+            ],
+        )
+
+        writer.writeheader()
+
+        x_value = None
+        min_values: Dict[str, float] = defaultdict(lambda: float("inf"))
+        max_values: Dict[str, float] = defaultdict(lambda: float("-inf"))
+
+        for line_num, line_dict in enumerate(reader):
+            for key, value in line_dict.items():
+                if key != x and key in ys:
+                    min_values[key] = min(min_values[key], float(value))
+                    max_values[key] = max(max_values[key], float(value))
+
+            if line_num % period == 0:
+                x_value = line_dict[x]
+
+            if line_num % period == period - 1:
+                writer.writerow(
+                    {
+                        **{x: x_value},
+                        **{f"{key}_min": value for key, value in min_values.items()},
+                        **{f"{key}_max": value for key, value in max_values.items()},
+                    }
+                )
+
+                min_values = defaultdict(lambda: float("inf"))
+                max_values = defaultdict(lambda: float("-inf"))
+
+        if line_num % period != period - 1:
+            writer.writerow(
+                {
+                    **{x: x_value},
+                    **{f"{key}_min": value for key, value in min_values.items()},
+                    **{f"{key}_max": value for key, value in max_values.items()},
+                }
+            )
+
+
+def sample_sampled(source_path: Path, dest_path: Path, period: int) -> None:
+    """Sample an already sampled CSV file every `period` line.
+
+    Example:
+    With the following CSV corresponding to `source_path`:
+    a,b_min,b_max,d_min,d_max
+    1,2.0,6.0,4.0,8.0
+    9,10.0,14.0,12.0,16.0
+    17,18.0,18.0,20.0,20.0
+
+    With period == 2,
+
+    The CSV file written in `dest_path` will be:
+    a,b_min,b_max,d_min,d_max
+    1,2.0,14.0,4.0,16.0
+    17,18.0,18.0,20.0,20.0
+    """
+    with source_path.open() as source_file, dest_path.open("w") as dest_file:
+        reader = csv.DictReader(source_file)
+
+        assert reader.fieldnames
+
+        field_names = reader.fieldnames
+
+        xs = [
+            item
+            for item in field_names
+            if not (item.endswith("_min") or item.endswith("_max"))
+        ]
+
+        if len(xs) == 0:
+            raise ValueError("No X found in source file")
+
+        x, *trash = xs
+
+        if trash:
+            raise ValueError("Several Xs found in source file")
+
+        writer = csv.DictWriter(dest_file, lineterminator="\n", fieldnames=field_names)
+        writer.writeheader()
+
+        x_value = None
+        min_values: Dict[str, float] = defaultdict(lambda: float("inf"))
+        max_values: Dict[str, float] = defaultdict(lambda: float("-inf"))
+
+        for line_num, line_dict in enumerate(reader):
+            for key, value in line_dict.items():
+                if key.endswith("_min"):
+                    min_values[key] = min(min_values[key], float(value))
+                elif key.endswith("_max"):
+                    max_values[key] = max(max_values[key], float(value))
+
+            if line_num % period == 0:
+                x_value = line_dict[x]
+
+            if line_num % period == period - 1:
+                writer.writerow({**{x: x_value}, **min_values, **max_values})
+                min_values = defaultdict(lambda: float("inf"))
+                max_values = defaultdict(lambda: float("-inf"))
+
+        if line_num % period != period - 1:
+            writer.writerow({**{x: x_value}, **min_values, **max_values})
+
+
+def pad(input_path: Path, output_path: Optional[Path] = None) -> None:
+    """Pad the text file (in place) pointed by `input_path` with white spaces.
+
+    input_path: The path of the input file
+    output_path: The path of the output file. If not provided, will replace the input
+                 file
+    """
+    with input_path.open() as lines:
+        line_lenghts = set(len(line.rstrip(os.linesep)) for line in lines)
+
+    if len(line_lenghts) == 1:
+        # Every lines have the same lenght, so we can skip.
+        # Warning: There is case where the length of two consecutive lines could sum up
+        #          to the unique value of line_lengths. We choose to skip this case for
+        #          now, and hope this case does not happen.
+        return
+
+    max_line_lenght = max(line_lenghts)
+
+    with NamedTemporaryFile("w") as dest_file:
+        with input_path.open() as source_lines:
+            for source_line in source_lines:
+                stripped_source_line = source_line.rstrip()
+                padding = max_line_lenght - len(stripped_source_line)
+                dest_file.write(f"{stripped_source_line}{' '*padding}\n")
+
+        dest_file.flush()
+        copy2(dest_file.name, output_path if output_path is not None else input_path)
 
 
 def pad_and_sample(
--- tests/test_pad_and_sample.py
+++ tests/test_pad_and_sample.py
@@ -1,22 +1,44 @@
 import filecmp
 import os
 from pathlib import Path
+from shutil import copy2
 
-from huge_csv_reader.pad_and_sample import pad_and_sample, pseudo_hash
+import pytest
+from huge_csv_reader.pad_and_sample import (
+    pad,
+    pad_and_sample,
+    pseudo_hash,
+    sample,
+    sample_sampled,
+)
 from pytest import fixture
 
 from tests import assets
 
 
-def test_pseudo_hash(tmp_path: Path):
-    path = tmp_path / "file.txt"
-    with path.open("w") as file_descriptor:
-        file_descriptor.write("qwerty")
+@fixture
+def padded_file_path() -> Path:
+    return Path(assets.__file__).parent / "padded.csv"
 
-    os.utime(path, (42, 42))
 
-    assert pseudo_hash(path) == "46141dc64506ddc74fcf3cae348e0d77"
-    assert pseudo_hash(path, "12345") == "ece35c8647c0fe39e450443c9514b412"
+@fixture
+def sampled_file_path() -> Path:
+    return Path(assets.__file__).parent / "sampled.csv"
+
+
+@fixture
+def double_sampled_file_path() -> Path:
+    return Path(assets.__file__).parent / "double_sampled.csv"
+
+
+@fixture
+def sampled_no_x_file_path() -> Path:
+    return Path(assets.__file__).parent / "sampled_no_x.csv"
+
+
+@fixture
+def sampled_multiple_x_file_path() -> Path:
+    return Path(assets.__file__).parent / "sampled_multiple_x.csv"
 
 
 @fixture
@@ -49,6 +71,87 @@ def _2(hashed: Path):
 @fixture
 def _3(hashed: Path):
     return hashed / "3.csv"
+
+
+def test_pad(tmpdir, not_padded_file_path, padded_file_path):
+    copy2(not_padded_file_path, tmpdir)
+
+    pad(tmpdir / "not_padded.csv")
+    assert filecmp.cmp(tmpdir / "not_padded.csv", padded_file_path)
+
+    pad(tmpdir / "not_padded.csv")
+    assert filecmp.cmp(tmpdir / "not_padded.csv", padded_file_path)
+
+
+def test_pseudo_hash(tmp_path: Path):
+    path = tmp_path / "file.txt"
+    with path.open("w") as file_descriptor:
+        file_descriptor.write("qwerty")
+
+    os.utime(path, (42, 42))
+
+    assert pseudo_hash(path) == "46141dc64506ddc74fcf3cae348e0d77"
+    assert pseudo_hash(path, "12345") == "ece35c8647c0fe39e450443c9514b412"
+
+
+def test_sample(tmpdir, not_padded_file_path, sampled_file_path):
+    sample(
+        not_padded_file_path,
+        tmpdir / "output.csv",
+        "a",
+        [("b", float), ("d", float)],
+        2,
+    )
+
+    assert filecmp.cmp(tmpdir / "output.csv", sampled_file_path)
+
+
+def test_sample_not_float(tmpdir, not_padded_file_path):
+    with pytest.raises(ValueError):
+        sample(
+            not_padded_file_path,
+            tmpdir / "output.csv",
+            "a",
+            [("b", str), ("d", float)],
+            2,
+        )
+
+
+def test_sample_bad_x(tmpdir, not_padded_file_path):
+    with pytest.raises(IndexError):
+        sample(
+            not_padded_file_path,
+            tmpdir / "output.csv",
+            "not_existing",
+            [("b", float), ("d", float)],
+            2,
+        )
+
+
+def test_sample_bad_y(tmpdir, not_padded_file_path):
+    with pytest.raises(IndexError):
+        sample(
+            not_padded_file_path,
+            tmpdir / "output.csv",
+            "a",
+            [("b", float), ("not_existing", float)],
+            2,
+        )
+
+
+def test_sample_sampled(tmpdir, sampled_file_path, double_sampled_file_path):
+    sample_sampled(sampled_file_path, tmpdir / "output.csv", 2)
+    assert filecmp.cmp(tmpdir / "output.csv", double_sampled_file_path)
+
+
+def test_sample_sampled_no_x(tmpdir, sampled_no_x_file_path):
+    with pytest.raises(ValueError):
+        sample_sampled(sampled_no_x_file_path, tmpdir / "output.csv", 2)
+
+
+def test_sample_sampled_multiple_x(tmpdir, sampled_multiple_x_file_path):
+    with pytest.raises(ValueError):
+        sample_sampled(sampled_multiple_x_file_path, tmpdir / "output.csv", 2)
 
 
 def test_pad_and_sample(
